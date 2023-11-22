01d915dff51d5c5a51a21cfac21e8954882a07aa
Saurabh Krishan Gulab <saurabhgulab@gmail.com>
2023-11-22 19:22:25 +0530

    updates made in home page

--- src/app/components/about.jsx
+++ src/app/components/about.jsx
@@ -1,28 +1,17 @@
-import React from "react";
 import Image from "next/image";
+import backgroundImage from "../images/xcsfdsfdasdasdsa.svg";
 const links = [
-  { name: "Open roles", href: "#" },
-  { name: "Internship program", href: "#" },
-  { name: "Our values", href: "#" },
-  { name: "Meet our leadership", href: "#" },
-];
-const stats = [
-  { name: "Offices worldwide", value: "12" },
-  { name: "Full-time colleagues", value: "300+" },
-  { name: "Hours per week", value: "40" },
-  { name: "Paid time off", value: "Unlimited" },
+  { name: "Web Design", href: "#" },
+  { name: "Development", href: "#" },
+  { name: "Branding", href: "#" },
+  { name: "Ecommerce", href: "#" },
+  { name: "Marketing", href: "#" },
+  { name: "SEO", href: "#" },
 ];
 
 const about = () => {
   return (
     <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
-      <Image
-        src=""
-        alt="About us image"
-        width={300}
-        height={300}
-        className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
-      />
       <div
         className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
         aria-hidden="true"
@@ -49,35 +38,23 @@ const about = () => {
       </div>
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-            Work with us
+          <h2 className="p-2 text-4xl font-bold tracking-tight text-white sm:text-6xl">
+            Grow your Business with us.
           </h2>
-          <p className="mt-6 text-lg leading-8 text-gray-300">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-            fugiat aliqua.
+          <p className="p-2 text-sm leading-8 text-white">
+            We help businesses in the digital management of their operations.
+            From e-commerce, to websites, to the right technology and marketing
+            campaigns - we take care of it all!
           </p>
         </div>
         <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
-          <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
+          <div className="p-2 grid grid-cols-2 gap-x-4 gap-y-6 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-24">
             {links.map((link) => (
               <a key={link.name} href={link.href}>
                 {link.name} <span aria-hidden="true">&rarr;</span>
               </a>
             ))}
           </div>
-          <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
-            {stats.map((stat) => (
-              <div key={stat.name} className="flex flex-col-reverse">
-                <dt className="text-base leading-7 text-gray-300">
-                  {stat.name}
-                </dt>
-                <dd className="text-2xl font-bold leading-9 tracking-tight text-white">
-                  {stat.value}
-                </dd>
-              </div>
-            ))}
-          </dl>
         </div>
       </div>
     </div>
--- src/app/components/features.jsx
+++ src/app/components/features.jsx
@@ -2,26 +2,34 @@ import {
   CloudArrowUpIcon,
   LockClosedIcon,
   ServerIcon,
+  ChartPieIcon,
 } from "@heroicons/react/20/solid";
 import Image from "next/image";
+import featuresImage from "../images/eb680f8bdc4031227e8ce78ee2a2d46b.svg";
 const data = [
   {
-    name: "Push to deploy.",
+    name: "Planning Strategy :",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
-    icon: CloudArrowUpIcon,
-  },
-  {
-    name: "SSL certificates.",
-    description:
-      "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
+      "Outline a comprehensive business plan that includes target market analysis, services offered, pricing strategy, and growth projections.",
     icon: LockClosedIcon,
   },
   {
-    name: "Database backups.",
+    name: "Design & Development :",
     description:
-      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+      "Develop a professional website that showcases your services, portfolio, team, testimonials, and contact information.",
     icon: ServerIcon,
+  },
+  {
+    name: "Launch Successfully :",
+    description:
+      "Implement a marketing strategy for your agency. Utilize digital channels such as social media, content marketing, and email campaigns to generate leads.",
+    icon: CloudArrowUpIcon,
+  },
+  {
+    name: "Client Relationship :",
+    description:
+      "Regularly communicate with clients to ensure their needs are met and expectations are exceeded. A robust onboarding process to provide exceptional customer service to build long-term relationships.",
+    icon: ChartPieIcon,
   },
 ];
 
@@ -32,23 +40,18 @@ const features = () => {
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4">
             <div className="lg:max-w-lg">
-              <h2 className="text-base font-semibold leading-7 text-indigo-600">
-                Deploy faster
-              </h2>
-              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                A better workflow
+              <p className="p-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                Have an idea but need help with development?
               </p>
               <p className="mt-6 text-lg leading-8 text-gray-600">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Maiores impedit perferendis suscipit eaque, iste dolor
-                cupiditate blanditiis ratione.
+                Our team can take you from concept to real-life product.
               </p>
               <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
                 {data.map((feature) => (
                   <div key={feature.name} className="relative pl-9">
                     <dt className="inline font-semibold text-gray-900">
                       <feature.icon
-                        className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
+                        className="absolute left-1 top-1 h-5 w-5 text-blue-600"
                         aria-hidden="true"
                       />
                       {feature.name}
@@ -60,8 +63,8 @@ const features = () => {
             </div>
           </div>
           <Image
-            src=""
-            alt="Product screenshot"
+            src={featuresImage}
+            alt="featuresImage"
             className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
             width={2432}
             height={1442}
