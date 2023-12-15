2fedbef4e6d1af2f23657acac6f3a9ca10e3ba85
Saurabh Krishan Gulab <saurabhgulab@gmail.com>
2023-12-15 19:00:59 +0530

    few updates made

--- src/app/components/banner.jsx
+++ src/app/components/banner.jsx
@@ -1,73 +1,53 @@
 "use client";
 
 import Image from "next/image";
-import bannerImage from "../images/ubkemajiioa2312jnnjdnfjn.svg";
+import bannerImage from "../images/banner-image-1.svg";
 import React from "react";
 
 const banner = () => {
   return (
-    <section class="bg-white dark:bg-gray-900">
-      <div class="grid max-w-screen-xl px-4 py-8 m-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div class="mr-auto place-self-center lg:col-span-7">
-          <a
-            href="#"
-            class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
-            role="alert"
-          >
-            <span class="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 mr-3">
-              New
-            </span>{" "}
-            <span class="text-sm font-medium">
-              Trellingfood is out! See what's new
-            </span>
-            <svg
-              class="ml-2 w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </a>
-          <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-            Where creativity meets strategy to elevate your presence in the
-            digital age.
-          </h1>
-          <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-7s00">
-            Here at Trellingfood we focus on businesses where technology,
-            innovation, and capital can unlock long-term value and drive
-            economic growth.
-          </p>
+    <div class="grid max-w-screen-xl px-4 py-4 m-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+      <div class="mr-auto place-self-center lg:col-span-7">
+        <h1 class="max-w-2xl mb-4 text-2xl font-extrabold tracking-tight leading-none md:text-3xl xl:text-5xl dark:text-white">
+          Where creativity meets strategy to elevate your presence in the
+          digital age.
+        </h1>
+        <p class="max-w-2xl mb-6 font-sans text-gray-800 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-7s00">
+          Here at Trellingfood we focus on businesses where technology,
+          innovation, and capital can unlock long-term value and drive economic
+          growth.
+        </p>
 
-          <a
-            href="/contact"
-            class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+        <a
+          href="/"
+          class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+        >
+          Learn more
+          <svg
+            class="w-5 h-5 ml-2 -mr-1"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            Learn more
-            <svg
-              class="w-5 h-5 ml-2 -mr-1"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </a>
-        </div>
-
-        <div class="hidden lg:m-6 lg:col-span-5 lg:flex">
-          <Image src={bannerImage} alt="bannerImage" width={600} height={300} />
-        </div>
+            <path
+              fill-rule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </a>
       </div>
-    </section>
+
+      <div class="hidden lg:m-6 lg:col-span-5 lg:flex">
+        <Image
+          src={bannerImage}
+          alt="featuresImage"
+          className="m-auto rounded-xl shadow-xl ring-2 ring-gray-400/10 sm:w-[57rem] md:auto lg:auto"
+          width={600}
+          height={200}
+        />
+      </div>
+    </div>
   );
 };
 
