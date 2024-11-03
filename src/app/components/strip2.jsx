3c125f59e35da02aad2602ac0ffc8f99c6436491
Saurabh Krishan Gulab <saurabhgulab@gmail.com>
2024-11-03 22:36:31 +0530

    random change

--- src/app/components/strip2.jsx
+++ src/app/components/strip2.jsx
@@ -7,8 +7,8 @@ const strip2 = () => {
     <>
       <div className="bg-black grid justify-items-center mt-6">
         <div className="m-4">
-          <h1 className="max-w-2xl p-2 text-2xl font-extrabold tracking-tight leading-none md:text-3xl xl:text-5xl text-white ">
-            <span className="text-emerald-500">Trusted</span> By
+          <h1 className="flex items-center justify-center max-w-2xl p-2 text-2xl font-extrabold tracking-tight leading-none md:text-3xl xl:text-5xl text-white ">
+            <span className="text-emerald-500 mx-3">Trusted</span> By
           </h1>
           <p className="max-w-2xl m-auto p-2 font-sans  md:text-lg lg:text-xl text-white">
             The World's Leading and Largest{" "}
